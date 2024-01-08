17c6e314f2cd1d700a319d775c3220a14c61aa46
tobias10kodehode <tobias10.kodehode@gmail.com>
2024-01-08 10:49:45 +0100

    Fix for component Price.
    Added styling for component Text.

--- src/components/perfumeText/PerfumeText.jsx
+++ src/components/perfumeText/PerfumeText.jsx
@@ -4,7 +4,7 @@ export const PerfumeText = () => {
   return (
     <>
     <div className="text_wrapper">
-      <h2 className="title">Perfume</h2>
+      <h2 className="title">PERFUME</h2>
       <p className="description">Gabrielle Essence Eau De Parfum</p>
       <p className="text">
         A floral, solar and voluptuous interpretation composed by Oliver Polge,
